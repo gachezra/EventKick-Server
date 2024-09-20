03feda3f5fa87403d42321c175dd46c8bb9b765e
Ezra Kipkoech <71008423+gachezra@users.noreply.github.com>
2024-09-20 10:45:04 +0000

    Coordinates added

--- model/eventsModel.js
+++ model/eventsModel.js
@@ -17,6 +17,14 @@ const eventSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  latitude: {
+    type: String,
+    required: true,
+  },
+  longitude: {
+    type: String,
+    required: true,
+  },
   image: {
     type: String,
     default: null,
